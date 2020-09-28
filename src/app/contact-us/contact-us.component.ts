e8dc5711a7ff57c4956f049441ae37e2e7221d4b
thiskartik <gautam.kartik05@gmail.com>
2020-09-28 12:02:42 +0530

    added working contactus page

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { FormArray, FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-contact-us',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  object;
+
+  constructor(public db: AngularFireDatabase) {
+
+    this.object = { name: 'kartik', subject: 'This is subject', message: 'This is a Message' }
+
+    // db.object('contactus/'+db.createPushId()).set(this.object).then(res => {
+    //   console.log(res)
+    // });
+
+    db.list('contactus').valueChanges().subscribe(res => console.log(res))
+
+  }
+
+  contact = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    contact: new FormControl(''),
+    subject: new FormControl(''),
+    message: new FormControl(''),
+  })
 
   ngOnInit(): void {
+  }
+
+  submit() {
+
+    this.object = { name: this.contact.get('name').value, email: this.contact.get('email').value, contact: this.contact.get('contact').value, subject: this.contact.get('subject').value, message: this.contact.get('message').value }
+
+    this.db.object('contactus/' + this.db.createPushId()).set(this.object).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+
   }
 
 }
