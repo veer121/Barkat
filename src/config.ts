97f6f6992d65068e1beb92a71d2a54eb73ca89bd
Kartik Gautam <gautam.kartik05@gmail.com>
2020-10-07 21:02:47 +0530

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const firebase = {
+    apiKey: "AIzaSyB-KTgS5cj410d8QSYGIB7ZV7Pa9tVT0wE",
+    authDomain: "barkat-cb375.firebaseapp.com",
+    databaseURL: "https://barkat-cb375.firebaseio.com",
+    projectId: "barkat-cb375",
+    storageBucket: "barkat-cb375.appspot.com",
+    messagingSenderId: "785271100885",
+    appId: "1:785271100885:web:6d653ef603b59c2aa9995c",
+    measurementId: "G-VL28ECF787"
+}
